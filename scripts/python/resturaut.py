96a080e8af098c672490f9ca39e7b431a55eb0de
Juhi Seshadri Iyer <juhiiyer@gmail.com>
2019-05-07 20:59:38 +0530

    Fix logic, spelling

--- scripts/python/resturaut.py
+++ scripts/python/resturaut.py
@@ -5,26 +5,61 @@ print('By the way my name is Mr.Ghosh.')
 print('First thing on the list is the menue.')
 print('pizza(cheeese) = $20')
 print('pasta(red) = $15')
-print('lazania(Extra Cheeeessyyy) =  $23')
+print('lasagna(Extra Cheeeessyyy) =  $23')
 print('garlic bread(with cheeese) = $10')
 print('POP corn(Extra cheeese) =  $25')
-popcorn = int(25)
-garlicbread = int(10)
-lazania = int(23)
-pasta = int(15)
-pizza = int(20)
+p_popcorn = 25
+p_garlicbread = 10
+p_lasagna = 23
+p_pasta = 15
+p_pizza = 20
+total = 0
+
+print (p_pasta)
+
 print('WARNING: type everything in lower case.')
 print('You can only have 1 items.')
 firstitem = input('Item 1: ')
 
-if firstitem == 'pizza' or 'pasta' or 'lazania' or 'popcorn' or 'garlic bread':
-	total = firstitem + int(8)
-	print('Your total bill with taxes is %s'%(total))
-	print('Thank you for ordering from our shop and bye.')
+
+if firstitem == 'pizza':
+    price = 20
+    total += price
+
+if firstitem == 'popcorn':
+    price = 25
+    total += price
+
+if firstitem == 'garlicbread':
+    price = 10
+    total += price
+
+if firstitem == 'lasagna':
+    price = 23
+    total += price
+
+if firstitem == 'pasta':
+    price = 15
+    total += price
+
+
+
+
+
+print('Your total is %s'%(total))
+
+
+
+'''
+
+if firstitem == 'pizza' or 'pasta' or 'lasagna' or 'popcorn' or 'garlic bread':
+    total = int('p_' + firstitem)
+    print('Your total bill with taxes is %s'%(total))
+    print('Thank you for ordering from our shop and bye.')
 else:
-	print('I think there is a mistake in your input. Please follow the instructions and run the program again')
+    print('I think there is a mistake in your input. Please follow the instructions and run the program again')
 
-
+'''
 
 
 
