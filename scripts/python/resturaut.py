bf1707f9b10dbb4e355022bede72b682854235e5
Juhi Seshadri Iyer <juhiiyer@gmail.com>
2019-05-09 22:09:06 +0530

    add tax, 2items
    added tax at 8%
    
    option for 2 items
    
    printed bill

--- scripts/python/resturaut.py
+++ scripts/python/resturaut.py
@@ -1,67 +1,62 @@
 #!/usr/bin/python3
-print('Welcome to my restaraunt, Little Bay.')
-print('I will be your servent who will serve you food on your door step.')
-print('By the way my name is Mr.Ghosh.')
-print('First thing on the list is the menue.')
-print('pizza(cheeese) = $20')
-print('pasta(red) = $15')
-print('lasagna(Extra Cheeeessyyy) =  $23')
-print('garlic bread(with cheeese) = $10')
-print('POP corn(Extra cheeese) =  $25')
-p_popcorn = 25
-p_garlicbread = 10
-p_lasagna = 23
-p_pasta = 15
-p_pizza = 20
+print ('Welcome to my restaraunt, Little Bay.')
+print ('I will be your servent who will serve you food on your door step.')
+print ('By the way my name is Mr.Ghosh.')
+print ('First thing on the list is the menue.')
+print ('pizza(cheeese) = $20')
+print ('pasta(red) = $15')
+print ('lazania Extra Cheeeessyyy =  $23')
+print ('garlic bread(with cheeese) = $10')
+print ('POP corn(Extra cheeese) =  $25')
+p_popcorn = int(25)
+p_garlicbread = int(10)
+p_lasagna = int(23)
+p_pasta = int(15)
+p_pizza = int(20)
 total = 0
-
-print (p_pasta)
-
 print('WARNING: type everything in lower case.')
-print('You can only have 1 items.')
+print ('You can only have 2 items.')
 firstitem = input('Item 1: ')
-
+second_item = input('Item 2: ')
 
 if firstitem == 'pizza':
-    price = 20
-    total += price
+    # print ('Your total is %s' %(p_pizza))
+    total = total + p_pizza
+elif firstitem == 'popcorn':
+    # print ('Your total is %s' %(p_popcorn))
+    total = total + p_popcorn
+elif firstitem == 'garlic bread':
+    # print ('Your total is %s' %(p_garlicbread))
+    total = total + p_garlicbread
+elif firstitem == 'lasagna':
+    # print ('Your total is %s' %(p_lasagna))
+    total = total + p_lasagna
+elif firstitem == 'pasta':
+    # print ('Your total is %s' %(p_pasta))
+    total = total + p_pasta
 
-if firstitem == 'popcorn':
-    price = 25
-    total += price
+if second_item == 'pizza':
+    # print ('Your total is %s' %(p_pizza))
+    total = total + p_pizza
+elif second_item == 'popcorn':
+    # print ('Your total is %s' %(p_popcorn))
+    total = total + p_popcorn
+elif second_item == 'garlic bread':
+    # print ('Your total is %s' %(p_garlicbread))
+    total = total + p_garlicbread
+elif second_item == 'lasagna':
+    # print ('Your total is %s' %(p_lasagna))
+    total = total + p_lasagna
+elif second_item == 'pasta':
+    # print ('Your total is %s' %(p_pizza))
+    total = total + p_pizza
 
-if firstitem == 'garlicbread':
-    price = 10
-    total += price
+print ('The sub total is %s' %(total))
 
-if firstitem == 'lasagna':
-    price = 23
-    total += price
+tax = 0.08 * total
 
-if firstitem == 'pasta':
-    price = 15
-    total += price
+print ('The tax at 8 percent is %s' %(tax))
 
+grand_total = total + tax
 
-
-
-
-print('Your total is %s'%(total))
-
-
-
-'''
-
-if firstitem == 'pizza' or 'pasta' or 'lasagna' or 'popcorn' or 'garlic bread':
-    total = int('p_' + firstitem)
-    print('Your total bill with taxes is %s'%(total))
-    print('Thank you for ordering from our shop and bye.')
-else:
-    print('I think there is a mistake in your input. Please follow the instructions and run the program again')
-
-'''
-
-
-
-
-    
+print ('The grand total is %s' %(grand_total))
