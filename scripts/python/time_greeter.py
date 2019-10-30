e15c2ba04f43123612d36fba473956c48c5f5a76
Juhi Iyer <juhiiyer@gmail.com>
2019-10-30 20:18:26 +0530

    check for integer input

--- scripts/python/time_greeter.py
+++ scripts/python/time_greeter.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3.7
 print('Welcome to the greeting centre')
-question = input('What is the time(only type the hour): ')
+question = int(input('What is the time(only type the hour): '))
 print ('You have entered %s'%(question))
+if type(question) == int:
+	print ('Good')
+else:
+	print ('There is a preschool on the gachibowli chauraha. They might have free education for you.')
 gm = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
