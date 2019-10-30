352e135845b3aa2a06b7abb4e28a8aa422061c42
Juhi Seshadri Iyer <juhiiyer@gmail.com>
2019-10-30 16:43:42 +0530

    first commit of file

---
+++ scripts/python/time_greeter.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3.6
+print('Welcome to the greeting centre')
+question = input('What is the time(only type the hour): ')
+print ('You have entered %s'%(question))
+gm = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
