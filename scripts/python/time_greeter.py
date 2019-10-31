839f439194f5867e4321596851ffec2d40ec503c
Juhi Seshadri Iyer <juhiiyer@gmail.com>
2019-10-31 16:45:20 +0530

    use try-except loop

--- scripts/python/time_greeter.py
+++ scripts/python/time_greeter.py
@@ -1,9 +1,23 @@
-#!/usr/bin/python3.7
+#!/usr/bin/python3.6
 print('Welcome to the greeting centre')
 question = int(input('What is the time(only type the hour): '))
+
+
+try:
+
+	value = int(question)
+	print('Yes, it is an integer')
+
+except ValueError:
+
+
+	print('No, it is not an integer')
+
+'''
 print ('You have entered %s'%(question))
 if type(question) == int:
 	print ('Good')
 else:
 	print ('There is a preschool on the gachibowli chauraha. They might have free education for you.')
 gm = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+'''
