f104588e399e614e30a78af688bb55eebcfbb81a
Juhi Seshadri Iyer <juhiiyer@gmail.com>
2019-12-11 18:34:36 +0530

    Introduction to dictionaries

---
+++ scripts/python/student_info.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.6
+information = {
+    "student1"      : {
+        "Name"      :"Juhi",
+        "Age"       :12,
+        "Gender"    :"Female"
+        },
+    "student2"      : {
+        "Name"      :"Arihant",
+        "Age"       :6,
+        "Gender"    :"Male"
+        }
+}
+
+
+
+# print (information["student1"]["Age"])
+print (information["student2"]["Gender"])
