e986aea586c9724237b0ccabf3d4454db65fa210
Juhi Seshadri Iyer <juhiiyer@gmail.com>
2019-12-19 17:42:16 +0530

    mosh variable example

---
+++ scripts/python/prog_with_mosh/variable-1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.6
+'''
+full_name = 'John Smith'
+age = 20
+is_new = True
+'''
+name = input('what is your name?: ')
+colour = input('What is you favourite colour?: ')
+print ("Your name is " + name + ". Your favourite colour is " + colour)
