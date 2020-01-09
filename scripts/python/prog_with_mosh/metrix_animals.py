20761e569c2451fa82d79382663f0de8ef1e8c36
Juhi Seshadri Iyer <juhiiyer@gmail.com>
2020-01-09 17:29:51 +0530

    initial commits

---
+++ scripts/python/prog_with_mosh/metrix_animals.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.7
+'''
+This program is used to show the different types of animals.
+'''
+ANIMALS = [
+    ['peakock', 'kiwi', 'pigeon'],
+    ['whale', 'dolphin', 'fish'],
+    ['tiger', 'kagaroo', 'dog']
+]
+for creatures in ANIMALS:
+    for i in creatures:
+        print(i)
