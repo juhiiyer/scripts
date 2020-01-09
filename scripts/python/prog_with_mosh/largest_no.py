3ffd87e20d87cd8a37492b977269b8a25343c839
Juhi Seshadri Iyer <juhiiyer@gmail.com>
2020-01-09 16:59:14 +0530

    multiple changes
    ran pylint
    added logic

--- scripts/python/prog_with_mosh/largest_no.py
+++ scripts/python/prog_with_mosh/largest_no.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3.7
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print('9')
+
+'''
+ this program finds the largest number and prints it out for you.
+'''
+NUMBERS = [1, 12, 663, 44, 25, 68, 733, 8777, 29]
+
+
+MAX_NUM = 0
+for number in NUMBERS:
+    if number >= MAX_NUM:
+        MAX_NUM = number
+
+print('The largest number is {}'.format(MAX_NUM))
