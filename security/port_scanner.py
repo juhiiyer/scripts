f0bc50814ad3ee16b3cefb20cce913ef8d73bebb
Juhi S Iyer <juhiiyer@gmail.com>
2021-05-11 02:56:10 -0400

    hardcode port numbers

--- security/port_scanner.py
+++ security/port_scanner.py
@@ -2,6 +2,8 @@
 import socket
 from IPy import IP
 
+lport = 79
+mport = 81
 
 def scan(targets):
     converted_ip = check_ip(targets)
@@ -42,10 +44,10 @@ def portscan(ipaddress, port):
         pass
 
 if __name__ == '__main__':
-    
+
     targets = input('(+) enter the target/s ip address (separate targets with ",") : ')
-    lport = int(input('(+) enter the minimum port number: '))
-    mport = int(input('(+) enter the maximum port number: '))
+    # lport = int(input('(+) enter the minimum port number: '))
+    # mport = int(input('(+) enter the maximum port number: '))
 
     if ',' in targets:
 
