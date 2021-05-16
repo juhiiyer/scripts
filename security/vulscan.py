c040cf8575575bbd7181dcd275d223ffd3cee526
Juhi S Iyer <juhiiyer@gmail.com>
2021-05-16 02:44:24 -0400

    read vulnerabilty banners from file

--- security/vulscan.py
+++ security/vulscan.py
@@ -6,4 +6,14 @@ port_number = int(input('Enter the number of ports you want to scan: '))
 vul_file = input('Enter the path of the file with the vulnerable software: ')
 print('\n')
 
-portscanner.scan(targets_ip)
+target = portscanner.PortScan(targets_ip, port_number)
+target.scan()
+
+with open(vul_file, 'r') as file:
+    count = 0
+    for banner in target.banners:
+        file.seek(0)
+        for line in file.readlines():
+            if line.strip() in banner:
+                print('[!!] VULNERABLE BANNER: " ' + banner + '" ON PORT: ' +str(target.open_ports[count]))
+        count += 1
