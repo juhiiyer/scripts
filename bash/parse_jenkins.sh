beefdba2a9eb5d1b121ab171f125f3049e9d9ca3
Juhi S Iyer <juhiiyer@gmail.com>
2021-11-19 17:13:19 +0530

    initial commit

--- bash/parse_jenkins.sh
+++ bash/parse_jenkins.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 file_name=$1
+if [[ $# -ne 1 ]]
+   then
+     echo "need exactly one argument"
+     exit 1
+fi
+
+if [[ ! -f $# ]]; then
+    echo "file doesnt exist."
+    exit 1
+fi
+
 awk '/STAR/ {print $2}' $file_name |awk -F: '{print $1}' >star.txt
 awk '/GUI/ {print $2}' $file_name  |awk -F: '{print $1}'>gui.txt
 echo 'ig its done'
