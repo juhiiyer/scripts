92f1fd23856adab9de776f9637e6143540de346f
Juhi Iyer <juhiiyer@gmail.com>
2025-10-01 11:47:18 -0400

    Initial commit

---
+++ bash/variables.sh
@@ -0,0 +1,9 @@
+#assigning variables in bash 
+country=Russia
+new_country=$country 
+echo $new_country
+
+#getting an input
+echo 'What is your name'
+read entered_name
+echo -e "\nyour name is " $entered_name
